e35182edf47673c4bcb73b0728d34314db9c6b51
Anderson Luiz Fernandes Jeronimo <anderson.luiz.sjc@gmail.com>
2017-05-11 23:46:27 -0300

    Inclusão de rotas do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { AudioComponent } from './audio/audio.component';
+import { VideoComponent } from './video/video.component';
+import { ImagesComponent } from './images/images.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { HomeComponent } from './home/home.component';
+import { routing } from "app/app.routing";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    AudioComponent,
+    VideoComponent,
+    ImagesComponent,
+    ArticlesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
